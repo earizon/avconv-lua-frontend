9e8055a36cb09b6583b77d1ef5f6b6407f040f07
EAB <enrique.arizonbenito@gmail.com>
2014-03-28 11:01:36 +0100

    Work on avconv.lua front-end

---
+++ libavconv.lua
@@ -0,0 +1,63 @@
+-- frontend lua to avconv 
+
+local debug = false
+local avf = {} -- avconvc frontend
+
+avf.loglevel = "error"
+
+function _runCommon(cmd)
+    common = ""
+    common = common .. "-loglevel " .. avf.loglevel .. " "
+    common = common .. "-y "
+    common = common .. cmd
+    return avconv.run(cmd) -- <-- This line invoques the LUA-C avconv wrapper
+end
+
+-- All following functions are just utility wrappers around _runCommon
+
+-- COMMON PARAMETERS:
+-- In the following function next parameters have the same mean for all functions:
+--    input    : "anything" that libav can read -local file, udp stream,...
+--    outputDir: any local server dir with write access to the lua engine
+function avf.createHLS(input, outputDir, outputPrefix, urlPrefix)
+    -- outputPrefix: The ".m3u8" index file without the .m3u8 suffix
+    --               Ex: the input filename without the (mp4|avi|...) prefix
+    -- urlPrefix: Will be added to the .m3u8. ex: http://streaming.mydomain.net/HLS/
+
+    -- TODO:(0) For VoD do not remove segments. 
+    --          For LiveContent let only 3-5 segments and then remove
+    cmd = "" 
+    cmd = cmd .. "-i " .. input .. " "
+    cmd = cmd .. "-flags +global_header "
+    cmd = cmd .. "-codec copy "
+    cmd = cmd .. "-map 0 -f segment "
+    cmd = cmd .. "-segment_time 20 "   -- TODO:(0) Parametrize (make it smaller for Live Content)
+    cmd = cmd .. "-segment_list_size 10 "
+    cmd = cmd .. "-segment_list ".. outputDir .."/".. outputPrefix .. ".m3u8 "
+    cmd = cmd .. "-segment_list_type hls "
+    cmd = cmd .. "-segment_list_entry_prefix ".. urlPrefix .." "
+    cmd = cmd .. "Videos/".. outputPrefix .."%03d.ts"
+    return _runCommon(cmd);
+end
+
+function avf.getThumbnail(input, outputDir, outputName)
+    -- outputName : thumbnail file name without the (.png) prefix
+
+    cmd = ""
+    cmd = cmd .. "-y "
+    cmd = cmd .. "-i " .. input .. " "
+    cmd = cmd .. "-ss 00:00:25.000 " -- TODO:(?) Arbitrary position in stream
+    cmd = cmd .. "-f image2 "
+    cmd = cmd .. "-vframes 1 "
+    cmd = cmd .. outputDir .. "/" .. outputName ..".png"
+    return _runCommon(cmd);
+end
+
+function avf.getMetadata(input)
+    -- TODO(1): /dev/null doesn't work on Windows. 
+    -- According to: http://stackoverflow.com/questions/313111/dev-null-in-windows
+    cmd = "-i " .. input .. " -codec copy -f mp4  /dev/null"
+    return _runCommon(cmd) -- seconds, width, height
+end
+
+return avf;
