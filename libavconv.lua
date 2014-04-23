a24b40afea555d87028931e51bba0a5ffbaa7d49
EAB <enrique.arizonbenito@gmail.com>
2014-04-23 12:14:42 +0200

    Fix bug in wrapper

--- libavconv.lua
+++ libavconv.lua
@@ -1,16 +1,19 @@
 -- frontend lua to avconv 
 
-local debug = false
+local debug = true;
 local avf = {} -- avconvc frontend
 
 avf.loglevel = "error"
 
 function _runCommon(cmd)
-    common = ""
+    local common = ""
     common = common .. "-loglevel " .. avf.loglevel .. " "
     common = common .. "-y "
     common = common .. cmd
-    return avconv.run(cmd) -- <-- This line invoques the LUA-C avconv wrapper
+    if debug then print("debug: avf._runCommon: cmd:" .. common ); end
+    local result = avconv.run(common) -- <-- This line invoques the LUA-C avconv wrapper
+    if debug==true then print("deleteme avconv.run finished @ avf._runCommon"); end
+    return result 
 end
 
 -- All following functions are just utility wrappers around _runCommon
@@ -28,6 +31,7 @@ function avf.createHLS(input, outputDir, outputPrefix, urlPrefix)
     --          For LiveContent let only 3-5 segments and then remove
     cmd = "" 
     cmd = cmd .. "-i " .. input .. " "
+    cmd = cmd .. "-bsf h264_mp4toannexb "
     cmd = cmd .. "-flags +global_header "
     cmd = cmd .. "-codec copy "
     cmd = cmd .. "-map 0 -f segment "
@@ -36,7 +40,7 @@ function avf.createHLS(input, outputDir, outputPrefix, urlPrefix)
     cmd = cmd .. "-segment_list ".. outputDir .."/".. outputPrefix .. ".m3u8 "
     cmd = cmd .. "-segment_list_type hls "
     cmd = cmd .. "-segment_list_entry_prefix ".. urlPrefix .." "
-    cmd = cmd .. "Videos/".. outputPrefix .."%03d.ts"
+    cmd = cmd .. "core/Videos/".. outputPrefix .."%03d.ts"
     return _runCommon(cmd);
 end
 
@@ -44,11 +48,8 @@ function avf.getThumbnail(input, outputDir, outputName)
     -- outputName : thumbnail file name without the (.png) prefix
 
     cmd = ""
-    cmd = cmd .. "-y "
-    -- man avconv: "-ss position: When used as input option seeks in this input file to position, when used as an output option 
-    -- decodes but discards input until the timestamps reach position. This is slower, but more accurate."
-    cmd = cmd .. "-ss 00:00:25.000 " -- TODO:(?) Arbitrary position in stream
     cmd = cmd .. "-i " .. input .. " "
+    cmd = cmd .. "-ss 00:00:25.000 " -- TODO:(?) Arbitrary position in stream
     cmd = cmd .. "-f image2 "
     cmd = cmd .. "-vframes 1 "
     cmd = cmd .. outputDir .. "/" .. outputName ..".png"
@@ -56,9 +57,12 @@ function avf.getThumbnail(input, outputDir, outputName)
 end
 
 function avf.getMetadata(input)
+    local config = require("config");
     -- TODO(1): /dev/null doesn't work on Windows. 
     -- According to: http://stackoverflow.com/questions/313111/dev-null-in-windows
-    cmd = "-i " .. input .. " -codec copy -f mp4  /dev/null"
+    cmd = "-i " .. input .. " -f mp4 "
+    cmd = cmd .. "-codec copy "
+    cmd = cmd .. config.nullFile;
     return _runCommon(cmd) -- seconds, width, height
 end
 
